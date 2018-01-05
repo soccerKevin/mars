720f9ae1bf615dc2383da37b2221ea61b5925d5f
soccerkevin <soccerkevint11@gmail.com>
2018-01-05 00:23:11 -0800

    add solution

---
+++ solution.rb
@@ -0,0 +1,8 @@
+# since each point is indexed with xy, these indexes show the path
+
+[{:poi=> :solution=>[70, 60, 50, 51, 41]},
+ {:poi=> :solution=>[70, 60, 50, 51, 52, 42, 32, 22]},
+ {:poi=> :solution=>[70, 80, 81, 82, 72]},
+ {:poi=> :solution=>[70, 60, 50, 51, 52, 53, 43]},
+ {:poi=> :solution=>[70, 60, 50, 51, 52, 53, 43, 33, 23, 13, 14, 15, 25]},
+ {:poi=> :solution=>[70, 80, 81, 82, 83, 84, 85, 75, 65]}]
