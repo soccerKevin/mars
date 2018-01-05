3003e04aac174c1b27bcc8b9401d374d038e8f82
soccerkevin <soccerkevint11@gmail.com>
2018-01-05 00:01:11 -0800

    working for 1 point

--- mars.rb
+++ mars.rb
@@ -8,6 +8,7 @@ class PathFinder
   def initialize(mars, start_x, start_y)
     @map = grid_to_map mars
     @start_point = @map[start_y][start_x]
+    @path_keys = []
   end
 
   def points_of_interest
@@ -23,35 +24,34 @@ class PathFinder
   end
 
   def find_paths
-    # path_trees = points_of_interest.map do |interesting_point|
-    #   tree_first_node = traverse @start_point, interesting_point
-    # end
-    pp "looking for: #{points_of_interest.first.i}"
+    path_trees = points_of_interest.map do |interesting_point|
+      tree_first_node = traverse @start_point, interesting_point
+    end
+
     path_tree = traverse @start_point, points_of_interest.first
+    paths = path_tree.path_to points_of_interest.first.i
+    paths = paths.extract_values
     binding.pry
   end
 
   # poi = point of interest
+  # returns a node
   def traverse(current_point, poi, current_path_key=[])
     # has this point been visited
     pp('visited') and return nil if current_path_key.include? current_point.i
 
     # add this point to the path
-    current_path_key << current_point.i
+    new_path = current_path_key + [current_point.i]
+    node = Node.new new_path
 
-    pp('not traversable') and return nil if !current_point.traversable?
-    node = Node.new current_point
-    node.path_key current_path_key
+    pp('not traversable') and return node if !current_point.traversable?
 
-    # if current_point is the interesting one,
+    # if current_point is the interesting one
     # mark it's node as interesting for shortest path checking
-    if current_point == poi
-      node.winner
+    if current_point.i == poi.i
       pp 'winner'
       return node
     end
-
-    binding.pry if current_point.i == 14
 
     pp 'checking a new node'
 
@@ -68,7 +68,7 @@ class PathFinder
     end.compact # compact to remove nils
 
     # check the next nodes
-    children.each{ |child| node.add_child traverse child, poi, current_path_key }
+    children.each{ |child| node.add_child traverse child, poi, new_path }
     node # ruby returns result of last line by default
 
   rescue Exception => e
@@ -99,31 +99,20 @@ class Point
 end
 
 class Node
-  attr_accessor :value
+  attr_accessor :value, :path_key
 
-  def initialize(value=nil)
-    raise "no value error" if value.nil?
-    @value = value
+  def initialize(path_key=[])
     @children = []
-    @winner = false
-    @path_key = false
+    @path_key = path_key
   end
 
   def add_child(child)
     @children.push child unless child.nil?
   end
 
-  def shotest_path_to_leaf
-
-  end
-
-  def path_key(path_key=nil)
-    return @path_key unless path_key
-    @path_key = path_key
-  end
-
-  def winner
-    @winner = true
+  def path_to(point_index)
+    return @path_key if @path_key.include? point_index
+    return @children.map{ |child| child.path_to point_index }
   end
 end
 
@@ -133,6 +122,31 @@ class Randomizer
 
   def self.char
     CHARS.sample
+  end
+end
+
+class Array
+  def flatten_blanks
+    each{ |elem| elem.flatten_blanks if elem.is_a?(Array) }
+    reject!{ |elem| elem.is_a?(Array) && elem.length < 1 }
+    flatten! if self.length < 2
+    self
+  end
+
+  def extract_values
+    @values = []
+    find_values
+    @values
+  end
+
+  def find_values
+    each do |elem|
+      if elem.first.is_a? Integer
+        @values.push elem
+      elsif elem.first.is_a? Array
+        @values.concat elem.extract_values
+      end
+    end
   end
 end
 
